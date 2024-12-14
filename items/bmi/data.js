ddb7270b8f7d949ce98d60d60efe9f1c42e7aa3e
Mr Seyed <moosavi.sayyed@gmail.com>
2024-12-15 01:00:39 +0330

    m

---
+++ items/bmi/data.js
@@ -0,0 +1,12 @@
+let BankName = "بانک";
+var CustNum = "44";
+var acName = "خدم";
+var acNum = "12";
+var acSheba = "IR1986";
+var acCity = "مشتاق";
+var QCard = "بله";
+var CardNum = "6037";
+var CardCVV2 = "555";
+var CardEXP = "1408/08";
+
+document.getElementById("dta1").innerHTML = BankName;
